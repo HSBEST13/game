ee3589a019e4fbbd022880a71493872681a38678
Artyom <hsbest14@yandex.ru>
2022-01-03 11:28:34 +0300

    Commit

--- main.py
+++ main.py
@@ -10,24 +10,25 @@ if __name__ == "__main__":
     hero = pygame.sprite.Sprite(sprites)
     hero.image = pygame.image.load("data//images//hero//right.png")
     hero.rect = hero.image.get_rect()
-    hero.rect.y = 500 - hero.image.get_rect()[3]
     hero.rect.x = 250
+    hero.rect.y = 500 - hero.image.get_rect()[3]
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            key_1 = pygame.key.get_pressed()
-            key_2 = pygame.key.get_pressed()
-            if key_1 == key_2:
-                if key_1[pygame.K_UP]:
-                    hero.rect.y -= 10
-                if key_1[pygame.K_DOWN]:
-                    hero.rect.y += 10
-                if key_1[pygame.K_LEFT]:
-                    hero.rect.x -= 10
-                if key_1[pygame.K_RIGHT]:
-                    hero.rect.x += 10
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            hero.rect.y -= 1
+        if pygame.key.get_pressed()[pygame.K_DOWN]:
+            hero.rect.y += 1
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            hero.image = pygame.image.load("data//images//hero//left.png")
+            hero.rect.y = 500 - hero.image.get_rect()[3]
+            hero.rect.x -= 1
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            hero.image = pygame.image.load("data//images//hero//right.png")
+            hero.rect.y = 500 - hero.image.get_rect()[3]
+            hero.rect.x += 1
         screen.fill((0, 0, 0))
         sprites.draw(screen)
         sprites.update()
