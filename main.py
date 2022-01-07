8a989a2a3f1d143e6232dffb676175ad6e3333d2
Artyom <hsbest14@yandex.ru>
2022-01-07 22:22:46 +0300

    Commit

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ screen = pygame.display.set_mode(size)
 if __name__ == "__main__":
     sprites = pygame.sprite.Group()  # Группа спрайтов
     hero = Hero(sprites)
+    fps = 100
+    clock = pygame.time.Clock()
     running = True
     while running:
         if pygame.key.get_pressed()[pygame.K_LEFT]:  # Бег влево
             hero.left_()
         if pygame.key.get_pressed()[pygame.K_RIGHT]:  # Бег вправо
             hero.right_()
+        if pygame.key.get_pressed()[pygame.K_UP]:  # Прыжок
+            hero.jump()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:  # Прыжок
-                    hero.jump()
         screen.fill((0, 0, 0))
         sprites.draw(screen)
         sprites.update()
+        clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
