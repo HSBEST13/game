eb80684fec709720f8683b5f43cf5e53acc87f6a
Artyom <hsbest14@yandex.ru>
2022-01-07 21:33:38 +0300

    Commit

---
+++ data/classes/hero.py
@@ -0,0 +1,46 @@
+import pygame
+
+
+class Hero(pygame.sprite.Sprite):
+    image = pygame.image.load("data//images//hero//right.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = Hero.image
+        self.rect = self.image.get_rect()
+        self.rect.x = 200
+        self.rect.y = 600 - self.image.get_rect()[3]
+        self.jump_flag = False
+        self.jump_count = 200
+        self.right = False
+        self.left = False
+
+    def left_(self):
+        self.right = False
+        self.left = True
+
+    def right_(self):
+        self.right = True
+        self.left = False
+
+    def jump(self):
+        self.jump_flag = True
+
+    def update(self):
+        if self.right:  # Если правая кнопка
+            self.rect.x += 1
+            self.image = pygame.image.load("data//images//hero//right.png")
+            self.right = False
+        if self.left:  # Если левая кнопка
+            self.image = pygame.image.load("data//images//hero//left.png")
+            self.rect.x -= 1
+            self.left = False
+        if self.jump_flag:  # Прыжок
+            if self.jump_count == 0:
+                self.jump_count = 200
+                self.jump_flag = False
+            if 0 < self.jump_count <= 100:
+                self.rect.y += 1
+            else:
+                self.rect.y -= 1
+            self.jump_count -= 1
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from data.classes.hero import Hero
 
 pygame.init()
 size = width, height = 1000, 600
@@ -6,42 +7,21 @@ screen = pygame.display.set_mode(size)
 
 
 if __name__ == "__main__":
-    jump_flag = False
-    jump_count = 200
     sprites = pygame.sprite.Group()
-    hero = pygame.sprite.Sprite(sprites)
-    hero.image = pygame.image.load("data//images//hero//right.png")
-    hero.rect = hero.image.get_rect()
-    hero.rect.x = 250
-    hero.rect.y = 600 - hero.image.get_rect()[3]
+    hero = Hero(sprites)
     running = True
     while running:
+        if pygame.key.get_pressed()[pygame.K_LEFT]:
+            hero.left_()
+        if pygame.key.get_pressed()[pygame.K_RIGHT]:
+            hero.right_()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    jump_flag = True
-                    hero.rect.y -= 1
-        if pygame.key.get_pressed()[pygame.K_LEFT]:
-            hero.image = pygame.image.load("data//images//hero//left.png")
-            hero.rect.y = 600 - hero.image.get_rect()[3]
-            hero.rect.x -= 1
-        if pygame.key.get_pressed()[pygame.K_RIGHT]:
-            hero.image = pygame.image.load("data//images//hero//right.png")
-            hero.rect.y = 600 - hero.image.get_rect()[3]
-            hero.rect.x += 1
+                    hero.jump()
         screen.fill((0, 0, 0))
-        if jump_flag:
-            if jump_count == 0:
-                jump_count = 200
-                jump_flag = False
-                continue
-            if jump_count <= 100:
-                hero.rect.y += 1
-            else:
-                hero.rect.y -= 1
-            jump_count -= 1
         sprites.draw(screen)
         sprites.update()
         pygame.display.flip()
