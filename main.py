b6ba3a69a2f552a5af415655fcc53235d6f2f432
Artyom <hsbest14@yandex.ru>
2022-01-08 21:56:25 +0300

    Commit

--- main.py
+++ main.py
@@ -22,13 +22,20 @@ if __name__ == "__main__":
     cursor = pygame.sprite.Sprite(sprites)
     cursor.image = pygame.image.load("data//images//buttons and windows//cursor.png")
     cursor.rect = cursor.image.get_rect()
+    dialog_window = DialogWindow(dialog_parts)
+    dialog_window.set_parameters(x=ctypes.windll.user32.GetSystemMetrics(0) // 2 - 100,
+                                 y=ctypes.windll.user32.GetSystemMetrics(1) // 2 - 200,
+                                 width=200, height=400,
+                                 background_color=(100, 102, 100))
     dialog_btn = DialogButtonExit(dialog_parts)
-    dialog_btn.set_pos(120, 50)
+    dialog_btn.set_pos(x=ctypes.windll.user32.GetSystemMetrics(0) // 2 + 70,
+                       y=ctypes.windll.user32.GetSystemMetrics(1) // 2 - 200)
     fps = 60
     clock = pygame.time.Clock()
     running = True
     main_music = pygame.mixer.Sound("data//music//main_window.wav")
     shoot_music = pygame.mixer.Sound("data//music//shoot.wav")
+    game_music = pygame.mixer.Sound("data//music//game_music.wav")
     main_music.play(-1)
     while running:
         if pygame.key.get_pressed()[pygame.K_a]:  # Бег влево
